3ca65d0677308d5d2bb5d7cf110de73673973054
Brian Grant <brian.grant@morningstar.com>
2020-12-13 11:44:15 -0600

    Fewer starter tests, add docs

--- test/string_calculator_test.py
+++ test/string_calculator_test.py
@@ -2,74 +2,26 @@ import pytest
 
 from src.string_calculator import calculate_string
 
+""" Starter unit test file for the String Calculator kata.
 
-# TODO:  change from returns to yields?
-# TODO:  write up some instructions for this file specifically using https://realpython.com/documenting-python-code/
+This unit tests below support the first step of the kata and is designed to get you started practicing the 
+"Three Rules". One at a time, remove the @pytest.mark.skip annotation and run pytest; the test should fail. Update the 
+calculate_string implementation so the test passes. Continue in this fashion until all three tests are passing.
 
+Once completing these, three tests, write your own tests to complete the String Calculator kata. 
+"""
 
 
 @pytest.mark.skip
-def test_an_empty_string_returns_zero():
+def test_an_empty_string_yields_zero():
     assert 0 == calculate_string('')
 
 
 @pytest.mark.skip
-def test_a_single_number_returns_that_value():
+def test_a_single_number_yields_that_value():
     assert 5 == calculate_string('5')
 
 
 @pytest.mark.skip
-def test_two_numbers_comma_delimited_return_the_sum():
+def test_two_numbers_comma_delimited_yield_the_sum():
     assert 14 == calculate_string('5,9')
-
-
-@pytest.mark.skip
-def test_two_numbers_newline_delimited_returns_the_sum():
-    assert 14 == calculate_string('5\n9')
-
-
-@pytest.mark.skip  # TODO change this to a parameterized test
-def test_three_numbers_delimited_either_way_returns_the_sum():
-    assert 23 == calculate_string('5\n9\n9')
-    assert 23 == calculate_string('5,9,9')
-
-# def test_negative_numbers_raise_an_exception():
-
-#
-# @Disabled
-# @Test
-# void negativeNumbersThrowAnException() {
-#     Stream.of('-1', '5,-1,9')
-#         .forEach(numString -> {
-# try {
-# testMe.calculateString(numString);
-# fail("an exception should have been thrown for " + numString);
-# } catch (IllegalArgumentException e) {
-# assertEquals("negative numbers aren't allowed:  -1", e.getMessage());
-# }
-# });
-# }
-#
-# @Disabled
-#  @Test
-# void numbersGreaterThan1000AreIgnored() {
-#     assertEquals(23, testMe.calculateString('5,9,1025,9'));
-# }
-#
-# @Disabled
-#  @Test
-# void aSingleCharDelimiterCanBeDefinedOnTheFirstLine() {
-#     assertEquals(23, testMe.calculateString('#\n5#9#9'));
-# }
-#
-# @Disabled
-#  @Test
-# void aMultiCharDelimiterCanBeDefinedOnTheFirstLine() {
-#     assertEquals(23, testMe.calculateString('[###]\n5###9###9'));
-# }
-#
-# @Disabled
-#  @Test
-# void manySingleOrMultiCharDelimitersCanBeDefined() {
-#     assertEquals(23, testMe.calculateString('[|||][$$]\n5|||9$$9'));
-# }
